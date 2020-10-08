ecbb82c98025068d8ed0816a546feb997b73a6f5
s18008 <s18008@std.it-college.ac.jp>
2020-10-08 16:08:40 +0900

    チャット機能作成

--- web会議ツール/camela.js
+++ web会議ツール/camela.js
@@ -1,8 +1,11 @@
 var  peer = new Peer({key:'dd0d447c-2117-4c67-9df1-ff02cca9c355',debug: 3});
 var localVideo = document.getElementById('local_video');
 var kaku = document.getElementById('kaku');
+var addVideo = document.getElementById('stream-add')
+var chat = document.getElementById('chat')
 var localStream;
 var roomName;
+var room;
 
 navigator.mediaDevices.getUserMedia({audio: true,video: true,}).then(function(stream){
   localVideo.srcObject = stream;
@@ -17,17 +20,38 @@ localStream = stream;
         return;
     }
 
-    const call = peer.joinRoom(roomName, {mode: 'sfu', stream: localStream});
-   /*({video: true, audio: true},
-     function(stream) { // for success case
-       console.log(stream);
-       localVideo.srcObject = stream;
-     },
-     function(err) { // for error case
-       console.log(err);
-     });*/
-	    }
+    room = peer.joinRoom(roomName, {mode: 'sfu', stream: localStream});
+    room.on('stream', function(stream){
+      const newVideo = document.createElement('video');
+      newVideo.srcObject = stream;
+      newVideo.playsInline = true;
+      newVideo.setAttribute('id', `${stream.peerId}`);
+      newVideo.setAttribute('style',"width: 320px; height: 240px; border: 1px solid black;");
+      addVideo.append(newVideo);
+      newVideo.play().catch(console.error);
+    });
+
+    room.on('peerLeave',function(peerid){
+      const remoteVideo = document.getElementById(`${peerid}`);
+     remoteVideo.srcObject = null;
+     remoteVideo.remove();
+  });
+
+   room.on('data',function(data){
+     chat.textContent += `${data.data}\n`;
+   });
+}
+
+function chatroom(){
+     if(!room){
+       return;
+     }
+     const chatword = document.getElementById('chat-text');
+     room.send(chatword.value);
+     chat.textContent += `${chatword.value}\n`;
+     chatword.value = '';
+}
 
       peer.on('open', function(){
-        kaku.innerText = peer.id;
+        kaku.innerText = kaku.value;
 });
